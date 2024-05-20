3bda4f09bd6a78e33b09f9e48404531138197f62
yasai-nodame <yasaisecret1@gmail.com>
2024-05-20 21:12:29 +0900

    進捗

--- ffmpeg_mp4.py
+++ ffmpeg_mp4.py
@@ -1,12 +1,28 @@
-import ffmpeg 
+# import ffmpeg 
 import os 
+from moviepy.editor import VideoFileClip
+import speech_recognition as sr
 
+import env
 
-soccer_mp4 = r"F:\test_ffmpeg\8K9co4OYRlP5APKk.mp4"
-soccer_mp4_1 = r"F:\test_ffmpeg\HFeBeDRL8I3M_2ir.mp4"
+soccer_mp4 = env.os.environ.get('MP4_FILE')
 
+mp4_wav = env.os.environ.get('WAV_FILE')
 
-def ffmpeg_translation(mp4_file):
-    input_file = ffmpeg.input(mp4_file)
-    
+# mp4ファイルをwavファイルに変換
+clip = VideoFileClip(soccer_mp4)
+clip.audio.write_audiofile(os.path.join(test_wav, 'output.wav'))
+
+# wavファイルからテキストを抽出
+r = sr.Recognizer()
+try:
+    with sr.AudioFile(mp4_wav) as source:
+        audio = r.record(source)
+        
+        text = r.recognize_google(audio, language='en-US')
+        # テキスト抽出
+        print(text)
+        
+except Exception as e:
+    print('エラーが出ました。', e)
 
